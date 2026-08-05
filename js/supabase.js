d51926d94143c2823922e1013bc6ee8385d49a98
IAnnonce <hello@iannonce.ch>
2026-08-05 17:02:14 +0200

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+// Connexion Supabase - IAnnonce
+
+const SUPABASE_URL = "https://udubddnueiupdjewkzyp.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_rnvBmTb9am3nY92vmpBeRA_AnpHkfL-";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
