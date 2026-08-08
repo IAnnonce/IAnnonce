86f3b27351b67c9f0a6d36de79a121a14867a50e
IAnnonce <hello@iannonce.ch>
2026-08-08 17:28:22 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // Connexion Supabase - IAnnonce
 const SUPABASE_URL = "https://udubddnueiupdjewkzyp.supabase.co";
-const SUPABASE_KEY = "sb_publishable_rnvBmTb9am3nY92vmpBeRA_AnpHkfL-";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkdWJkZG51ZWl1cGRqZXdrenlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5MDk4NjYsImV4cCI6MjEwMTQ4NTg2Nn0.zHQM_cP5Ts7-6N2N2XbhSfghMGkZGvpTK2hHfGV5P3U";
 
 // On garde createClient, puis on remplace le global par le client
 const { createClient } = window.supabase;
